d200c9fbbd27b5a6222e7ea3d17588c0f776d54c
Marco Alvarado <marcoantonioam9@gmail.com>
2015-04-11 12:55:16 -0700

    Added token class.

---
+++ src/Token.java
@@ -0,0 +1,15 @@
+/**
+ * Created by drumarcco on 11/04/15.
+ * Time: 02:16 AM
+ */
+public class Token {
+    public String lexeme;
+    public int tableValue;
+    public int lineNumber;
+
+    public Token(String lexeme, int tableValue, int lineNumber){
+        this.lexeme = lexeme;
+        this.tableValue = tableValue;
+        this.lineNumber = lineNumber;
+    }
+}
