894888223c931454524ab3b00d684987a8b2fb7b
Marco Alvarado <marcoantonioam9@gmail.com>
2015-05-20 11:52:38 -0700

    Refactored code to separate GUI from compiler logic

--- src/LexicalAnalysis.java
+++ src/LexicalAnalysis.java
@@ -1,9 +1,106 @@
+import javax.swing.*;
+import java.util.LinkedList;
+
 /**
  * Created by drumarcco on 20/05/15.
  * Time: 11:19 AM
  */
 public class LexicalAnalysis {
-    public LexicalAnalysis() {
+    public LinkedList<Token> tokenList;
+    private LexicalStatesTable lexicalStatesTable;
+    public String fileString = "";
 
+    public LexicalAnalysis() {
+        lexicalStatesTable = lexicalStatesTable.getInstance();
+    }
+
+    public void generateTokens(){
+        tokenList = new LinkedList<>();
+        int lineNumber = 1;
+        int state = 0;
+        String lexeme = "";
+        for (int i=0; i<fileString.length(); i++){
+            char currentChar = fileString.charAt(i);
+            if (currentChar == '\n') lineNumber++;
+            String key = checkAlphaDigit(currentChar);
+
+            int tableValue = lexicalStatesTable.getTableValue(state, key);
+
+            if (tableValue == 0){
+                state = 0;
+                lexeme = "";
+            } else if (tableValue < 100){
+                lexeme += currentChar;
+                state = tableValue;
+            } else if (tableValue >= 100 && tableValue < 500){
+                if (isIndexDecrementRequired(tableValue)){
+                    i--;
+                } else {
+                    lexeme += currentChar;
+                }
+                if (tableValue == 100) tableValue = searchKeyword(lexeme);
+                Token token = new Token(lexeme, tableValue, lineNumber);
+                addTokenToList(token);
+                state = 0;
+                lexeme = "";
+            } else if (tableValue >= 500) {
+                printError(tableValue, lineNumber);
+                break;
+            }
+        }
+    }
+
+    private String checkAlphaDigit(char character){
+        if (Character.isLetter(character) || character == '_') return "Alpha";
+        if (Character.isDigit(character)) return "Digit";
+        return character + "";
+    }
+
+    private boolean isIndexDecrementRequired(int state){
+        switch (state){
+            case 100: return true;
+            case 101: return true;
+            case 102: return true;
+            case 103: return true;
+            case 108: return true;
+            case 111: return true;
+            case 113: return true;
+            case 115: return true;
+        }
+        return state >= 200 && state < 500;
+    }
+
+    private int searchKeyword(String lexeme){
+        switch (lexeme){
+            case "if": return 200;
+            case "else": return 201;
+            case "while": return 202;
+            case "return": return 203;
+            case "break": return 204;
+            case "goto": return 205;
+            case "next": return 206;
+            case "auto": return 207;
+            case "extrn": return 208;
+            case "main": return 209;
+            default: return 100;
+        }
+    }
+
+    private void addTokenToList(Token token){
+        tokenList.add(token);
+    }
+
+    private void printError(int errorNumber, int lineNumber){
+        String message = getError(errorNumber) + "\nLinea: " + lineNumber;
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+    private String getError(int errorNumber){
+        switch (errorNumber){
+            case 500: return "Error 500. Caracter no válido.";
+            case 501: return "Error 501. Fin de archivo inesperado.";
+            case 502: return "Error 502. Nueva linea inesperada.";
+            default: return "Error " + errorNumber + ".";
+        }
     }
 }
--- src/frame_home.java
+++ src/frame_home.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
 
 /**
  * Created by drumarcco on 10/04/15.
@@ -17,16 +16,14 @@ public class frame_home extends JFrame {
     private JButton btn_open;
     private JTextArea txt_output;
     private JButton btn_read;
-    private LexicalStatesTable lexicalStatesTable;
-    private LinkedList<Token> tokenList;
-    private String fileString = "";
+    private LexicalAnalysis lexicalAnalysis;
 
     public frame_home(){
         super("B Compiler Lexical Analysis");
         initializeFrame();
+        lexicalAnalysis = new LexicalAnalysis();
         addListeners();
 
-        lexicalStatesTable = LexicalStatesTable.getInstance();
     }
 
     private void initializeFrame(){
@@ -40,12 +37,12 @@ public class frame_home extends JFrame {
         txt_input.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent e) {
-                fileString = txt_input.getText() + "\n";
+                lexicalAnalysis.fileString = txt_input.getText() + "\n";
             }
 
             @Override
             public void removeUpdate(DocumentEvent e) {
-                fileString = txt_input.getText() + "\n";
+                lexicalAnalysis.fileString = txt_input.getText() + "\n";
             }
 
             @Override
@@ -57,7 +54,7 @@ public class frame_home extends JFrame {
         btn_open.addActionListener(e -> selectFile());
         btn_read.addActionListener(e -> {
             txt_output.setText("");
-            generateTokens();
+            lexicalAnalysis.generateTokens();
             printTokenList();
         });
     }
@@ -72,7 +69,7 @@ public class frame_home extends JFrame {
     }
 
     private void readFile(File file){
-        fileString = "";
+        lexicalAnalysis.fileString = "";
         FileReader fr;
         BufferedReader br = null;
         try {
@@ -80,10 +77,10 @@ public class frame_home extends JFrame {
             br = new BufferedReader(fr);
             String line;
             while ((line = br.readLine()) != null){
-                fileString += line + "\n";
+                lexicalAnalysis.fileString += line + "\n";
             }
-            txt_input.setText(fileString);
-            generateTokens();
+            txt_input.setText(lexicalAnalysis.fileString);
+            lexicalAnalysis.generateTokens();
             printTokenList();
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -98,104 +95,13 @@ public class frame_home extends JFrame {
         }
     }
 
-    private void generateTokens(){
-        tokenList = new LinkedList<>();
-        int lineNumber = 1;
-        int state = 0;
-        String lexeme = "";
-        for (int i=0; i<fileString.length(); i++){
-            char currentChar = fileString.charAt(i);
-            if (currentChar == '\n') lineNumber++;
-            String key = checkAlphaDigit(currentChar);
-
-            int tableValue = lexicalStatesTable.getTableValue(state, key);
-
-            if (tableValue == 0){
-                state = 0;
-                lexeme = "";
-            } else if (tableValue < 100){
-                lexeme += currentChar;
-                state = tableValue;
-            } else if (tableValue >= 100 && tableValue < 500){
-                if (isIndexDecrementRequired(tableValue)){
-                    i--;
-                } else {
-                    lexeme += currentChar;
-                }
-                if (tableValue == 100) tableValue = searchKeyword(lexeme);
-                Token token = new Token(lexeme, tableValue, lineNumber);
-                addTokenToList(token);
-                state = 0;
-                lexeme = "";
-            } else if (tableValue >= 500) {
-                printError(tableValue, lineNumber);
-                break;
-            }
-        }
-    }
-
-    private void printError(int errorNumber, int lineNumber){
-        String message = getError(errorNumber) + "\nLinea: " + lineNumber;
-        JOptionPane.showMessageDialog(this, message);
-    }
-
-    private String getError(int errorNumber){
-        switch (errorNumber){
-            case 500: return "Error 500. Caracter no válido.";
-            case 501: return "Error 501. Fin de archivo inesperado.";
-            case 502: return "Error 502. Nueva linea inesperada.";
-            default: return "Error " + errorNumber + ".";
-        }
-    }
-
-    private boolean isIndexDecrementRequired(int state){
-        switch (state){
-            case 100: return true;
-            case 101: return true;
-            case 102: return true;
-            case 103: return true;
-            case 108: return true;
-            case 111: return true;
-            case 113: return true;
-            case 115: return true;
-        }
-        return state >= 200 && state < 500;
-    }
-
     private void printTokenList(){
         txt_output.setText("");
-        for(Token token : tokenList){
+        for(Token token : lexicalAnalysis.tokenList){
             String line = "Token: ";
             line += token.tableValue + " ";
             line += token.lexeme;
             txt_output.append(line + "\n");
         }
     }
-
-    private String checkAlphaDigit(char character){
-        if (Character.isLetter(character) || character == '_') return "Alpha";
-        if (Character.isDigit(character)) return "Digit";
-        return character + "";
-    }
-
-    private int searchKeyword(String lexeme){
-        switch (lexeme){
-            case "if": return 200;
-            case "else": return 201;
-            case "while": return 202;
-            case "return": return 203;
-            case "break": return 204;
-            case "goto": return 205;
-            case "next": return 206;
-            case "auto": return 207;
-            case "extrn": return 208;
-            case "main": return 209;
-            default: return 100;
-        }
-    }
-
-    private void addTokenToList(Token token){
-        tokenList.add(token);
-    }
-
 }
